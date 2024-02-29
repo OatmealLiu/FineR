a28dda50074b3407e748397b7f2ab1b8fdf71141
mliu <mingxuanliu.miu@gmail.com>
2024-02-29 10:08:59 +0100

    FineR-release

--- batch_launcher_fullpipe.sh
+++ batch_launcher_fullpipe.sh
@@ -1,13 +1,13 @@
-folder_path="scripts_eval"
+folder_path="scripts_full_pipeline"
 for file in "$folder_path"/*.sh; do
     if [ -f "$file" ]; then
-        sh "$file"
+        sbatch "$file"
     fi
 done
 
-folder_path="scripts_eval_random"
+folder_path="scripts_full_pipeline_random"
 for file in "$folder_path"/*.sh; do
     if [ -f "$file" ]; then
-        sh "$file"
+        sbatch "$file"
     fi
 done
